8785622e3f0686799f583825954027ab6cf8a6a4
bappasahabapi <bappasahabapi@gmail.com>
2021-10-31 23:40:10 +0600

    update finally

--- src/pages/Booking/Booking/Booking.js
+++ src/pages/Booking/Booking/Booking.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import "./Booking.css";
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import axios from 'axios';
 
 
 
@@ -22,18 +24,42 @@ const Booking = () => {
 
     }, []);
 
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+
+        //post related code using axios ---------->
+        axios.post('https://whispering-beyond-39369.herokuapp.com/services', data)
+            .then(response => {
+                if (response.data.insertedId) {
+                    alert('Your booking is succesfully added');
+                    reset();
+                }
+
+            })
+        //---------------------->
+    }
+
     return (
-        <div className="booking mb-5">
-            <br /> <br />
-            <h3 className="fw-bolder  "> {service?.name} SERVICE IS BOOKED .  </h3>
-            <h3 className="fw-bolder  ">  Click 👇 to continue  </h3>
-            <Link to="/tourplan">
-                <button className="btn btn-warning   mt-2 ms-3 mb-4 fw-bold p-3 rounded-pill"> 🟢 Place Order ▶▶  </button>
-            </Link> <br />
-            <img className="w-25  rounded-3 " src={service?.img} alt="" /> <br />
+        <div className="booking mb-5 tour">
             <Link to="/">
-                <button className="btn btn-warning mt-4 mb-4 fw-bold p-3 rounded-pill">◀◀ Go Back 🔴</button>
+                <button className="btn btn-warning mt-2 mb-2 fw-bold p-3 rounded-pill">◀◀ Go Back 🔴</button>
             </Link>
+            <br />
+            <h5 className="fw-bolder  "> {service?.name} SERVICE IS BOOKED .  </h5>
+            <h5 className="fw-bolder  ">  Click 👇 to continue  </h5>
+            {/* <Link to="/tourplan">
+                <button className="btn btn-warning   mt-2 ms-3 mb-4 fw-bold p-3 rounded-pill"> 🟢 Place Order ▶▶  </button>
+            </Link> <br /> */}
+            <form className="m-0" onSubmit={handleSubmit(onSubmit)}>
+                <img className="w-25  rounded-3 mb-2 " src={service?.img} alt="" />
+                <small>type this <b>[  {service?.name}]</b> as package name </small>  <input className="fw-bold mt-3" {...register("name", { required: true, maxLength: 20 })} placeholder="package name" />
+                <textarea className="fw-bold" {...register("description", { required: true })} placeholder=" Email  or   Address " />
+                <input className="fw-bold" type="number" {...register("price", { required: true })} placeholder=" Your Contact Number" />
+                <small><b>type this 👇 showing url </b></small> <input className="fw-bold" {...register("img", { required: false })} placeholder={service?.img} />
+                <input className="bg-warning w-25 rounded-pill p-2 border-0 fw-bold fs-4" type="submit" />
+            </form>
+
 
             <br />
 
